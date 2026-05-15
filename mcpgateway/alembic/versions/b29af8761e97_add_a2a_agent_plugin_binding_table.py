60c3c8f6357dd6a62d63defed882cd724f52e886
Mohan Lakshmaiah <mohan.economist@gmail.com>
2026-05-15 15:11:36 +0530

    fix: add pragma allowlist secret to migration revision IDs and replace triple-quoted string with comment
    Signed-off-by: Mohan Lakshmaiah <mohan.economist@gmail.com>

--- mcpgateway/alembic/versions/b29af8761e97_add_a2a_agent_plugin_binding_table.py
+++ mcpgateway/alembic/versions/b29af8761e97_add_a2a_agent_plugin_binding_table.py
@@ -19,8 +19,8 @@ from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-revision: str = "b29af8761e97"
-down_revision: Union[str, Sequence[str], None] = "351b43e1d273"
+revision: str = "b29af8761e97"  # pragma: allowlist secret
+down_revision: Union[str, Sequence[str], None] = "351b43e1d273"  # pragma: allowlist secret
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
